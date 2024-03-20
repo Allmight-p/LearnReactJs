045c7b8ba30e113c05005e16a0493d03ea2dc2d3
allmight-p <imnaveene@gmail.com>
2024-03-20 11:53:40 +0530

    Completed posting bookings

--- we-care-project/src/Components/UserHomeComponent.jsx
+++ we-care-project/src/Components/UserHomeComponent.jsx
@@ -12,6 +12,7 @@ const UserHome = () => {
     const [appointments, setappointments] = useState([]);
     const loginid = useSelector(state => state.coachReducer.loginid);
     const [displayProfile, setdisplayProfile] = useState("");
+    const [coachid, setcoachid] = useState("");
     const [bookingform, setbookingform] = useState({appointmentDate : "", slot : "", userId : "", coachId : ""});
     const [bookingerrorform, setbookingerrorform] = useState({appointmentDate : "", slot : "", userId : "", coachId : ""});
 
@@ -26,6 +27,12 @@ const UserHome = () => {
             .then((res) => setappointments(res.data))
             .catch((err) => console.log(err));
         }
+        else if(displayProfile === "BookingSuccess"){
+            axios.post("http://localhost:8080/bookings/",bookingform)
+            .then((res) => {console.log(res.data.id)})
+            .catch((err) => {console.log(err); setdisplayProfile("Booking"); setbookingerrorform({...bookingerrorform, slot : "Posting failed"})})
+        }
+        else if(displayProfile === "Booking"){}
         else{
             axios.get("http://localhost:8080/coaches/")
             .then((res) => {setcoaches(res.data)})
@@ -40,7 +47,9 @@ const UserHome = () => {
             setbookingerrorform({...bookingerrorform, slot: sloterror, appointmentDate: appointmentdateerror});
         }
         else{
-            
+            setbookingerrorform({appointmentDate : "", slot : "", userId : "", coachId : ""});
+            setbookingform({...bookingform, userId : loginid, coachId : coachid})
+            setdisplayProfile("BookingSuccess");
         }
     }
 
@@ -91,7 +100,7 @@ const UserHome = () => {
                             <h3>Coach Id : {details.id}</h3>
                             <h4>Mobile No : {details.mobileNumber}</h4>
                             <h5>Speciality : {details.speciality}</h5>
-                            <button className="btn btn-primary" onClick={() => {setdisplayProfile("Booking")}}>Book An Appointment</button>
+                            <button className="btn btn-primary" onClick={() => {setcoachid(details.id); setdisplayProfile("Booking")}}>Book An Appointment</button>
                             </div>                        
                         );})
         )
@@ -101,24 +110,38 @@ const UserHome = () => {
         return(
             <div className="appointments">
                 <h2>Proceed with your Appointment</h2>
-                <form>
+                <form onSubmit={handleFormSubmission}>
                     <label htmlFor="Doa">Date of Appointment :</label><br />
                     <input type="date" name="Doa" onChange={(e) => {setbookingform({...bookingform, appointmentDate: e.target.value})}} />
                     <br />
                     <label htmlFor="slot">Preffered Slot :</label><br />
-                    <label htmlFor="slot">9AM to 10 AM :</label>
                     <input type="radio" name="slot" value="9AM to 10 AM" onChange={(e) => setbookingform({...bookingform, slot: e.target.value})} />
-                    <label htmlFor="slot">10AM to 11 AM :</label>
+                    <label htmlFor="slot">9AM to 10 AM :</label>
                     <input type="radio" name="slot" value="10AM to 11 AM" onChange={(e) => setbookingform({...bookingform, slot: e.target.value})} />
-                    <label htmlFor="slot">11AM to 12 PM :</label>
+                    <label htmlFor="slot">10AM to 11 AM :</label>
                     <input type="radio" name="slot" value="11AM to 12 PM" onChange={(e) => setbookingform({...bookingform, slot: e.target.value})} />
-                    <label htmlFor="slot">12PM to 1 PM :</label>
+                    <label htmlFor="slot">11AM to 12 PM :</label>
                     <input type="radio" name="slot" value="12PM to 1 PM" onChange={(e) => setbookingform({...bookingform, slot: e.target.value})} />
-                    <label htmlFor="slot">1PM to 2 PM :</label>
+                    <label htmlFor="slot">12PM to 1 PM :</label>
                     <input type="radio" name="slot" value="1PM to 2 PM" onChange={(e) => setbookingform({...bookingform, slot: e.target.value})} />
+                    <label htmlFor="slot">1PM to 2 PM :</label>
                     <br />
+                    {bookingerrorform.appointmentDate && <span className="text-danger">{bookingerrorform.appointmentDate}</span>}
+                    {bookingerrorform.slot && <span className="text-danger">{bookingerrorform.slot}</span>}
                     <button type="submit" className="btn btn-success">Confirm your appointment</button>
                 </form>
+            </div>
+        )
+    }
+
+    const ViewBookingSuccess =() => {
+        return(
+            <div className="appointments">
+                <h3>Your appointment is scheduled successfully</h3>
+                <br />
+                <div>
+                <button className="btn btn-info" onClick={() => {setdisplayProfile(""); navigate("/userhome")}}>Go Back</button>
+                </div>
             </div>
         )
     }
@@ -131,6 +154,8 @@ const UserHome = () => {
                 return ViewAppointment();
             case "Booking":
                 return ViewAppointmentBookings();
+            case "BookingSuccess":
+                return ViewBookingSuccess();
                
 
             default:
@@ -146,58 +171,7 @@ const UserHome = () => {
                 {
                     <>
                     {renderSwitch(displayProfile)}
-                    </>
-                    // displayProfile === "Viewprofile" ?
-                    // userprofile.map((details) =>{
-                    //     return(
-                    //         <div  id="appointments">
-                    //         <h2>{details.name}</h2>
-                    //         <h3>Date of Birth : {details.dateOfBirth}</h3>
-                    //         <h3>Email : {details.email}</h3>
-                    //         <h3>Mobile No : {details.mobileNumber}</h3>
-                    //         <h3>Address : {details.city + ", " + details.state}</h3>
-                    //         <h3>Pincode : {details.pincode}</h3>
-                    //         <button className="btn btn-info" onClick={() => {setdisplayProfile(false); navigate("/userhome")}}>Go Back</button>
-                    //         </div>
-                    //     );
-                    // })
-                    // :
-                    // (displayProfile === "Appointment" ?
-                    //     <div>                            
-                    //         {
-                    //             appointments.map((details) => {
-                    //                 return(<div className="appointments">
-                    //                <h2>Appointment Date : {details.appointmentDate}</h2> 
-                    //                <h3>Slot : {details.slot}</h3>
-                    //                <h4>Booking id : {details.id}</h4>
-                    //                <h4>User id : {details.userId}</h4>
-                    //                <h4>Coach id : {details.coachId}</h4>
-                    //                <button className="btn btn-info">Reschedule Appointment</button>
-                    //                <button className="btn btn-danger">Cancel Appointment</button>
-                    //                </div>
-                    //             );})
-                    //         }
-                    //         <button className="btn btn-info" onClick={() => {setdisplayProfile(""); navigate("/userhome")}}>Go Back</button>
-                    //     </div>
-                    //     :
-                    //     <>
-                    // <div>
-                    //     {
-                    //         coaches.map((details) => {
-                    //             return(
-                    //             <div className="userappointments" key={details.id}>
-                    //             <h2>{details.name}</h2>
-                    //             <h3>Coach Id : {details.id}</h3>
-                    //             <h4>Mobile No : {details.mobileNumber}</h4>
-                    //             <h5>Speciality : {details.speciality}</h5>
-                    //             <button className="btn btn-primary">Book An Appointment</button>
-                    //             </div>                        
-                    //         );})
-                    //     }
-                    // </div>
-                    // </>
-                    // )
-                    
+                    </>                    
                 }
             </div>
         </>
